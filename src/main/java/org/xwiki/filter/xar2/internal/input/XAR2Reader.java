2fe62bfc43279e9fc3364ba56435585e29044998
Jean SIMARD <woshilapin@tuziwo.info>
2015-07-23 14:54:19 +0300

    Try to create a simple document

--- src/main/java/org/xwiki/filter/xar2/internal/input/XAR2Reader.java
+++ src/main/java/org/xwiki/filter/xar2/internal/input/XAR2Reader.java
@@ -21,6 +21,7 @@ package org.xwiki.filter.xar2.internal.input;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -30,6 +31,7 @@ import org.slf4j.Logger;
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.InstantiationStrategy;
 import org.xwiki.component.descriptor.ComponentInstantiationStrategy;
+import org.xwiki.filter.FilterEventParameters;
 import org.xwiki.filter.FilterException;
 import org.xwiki.filter.event.model.WikiDocumentFilter;
 import org.xwiki.filter.input.InputSource;
@@ -43,38 +45,76 @@ import org.xwiki.logging.marker.TranslationMarker;
  */
 @Component(roles = XAR2Reader.class)
 @InstantiationStrategy(ComponentInstantiationStrategy.PER_LOOKUP)
-public class XAR2Reader
- {
-    @Inject
-    private Logger logger;
+public class XAR2Reader {
+	@Inject
+	private Logger logger;
 
-    private XAR2InputProperties properties;
+	private XAR2InputProperties properties;
 
-    public void setProperties(XAR2InputProperties properties)
-    {
-        this.properties = properties;
-    }
+	public void setProperties(XAR2InputProperties properties) {
+		this.properties = properties;
+	}
 
-    public void read(Object filter, XAR2InputFilter proxyFilter)
-    {    
-        InputStream stream;
-        InputSource source = this.properties.getSource();
-        if (source instanceof InputStreamInputSource) {
-            try {
+	public void read(Object filter, XAR2InputFilter proxyFilter) {
+		InputStream stream;
+		InputSource source = this.properties.getSource();
+		if (source instanceof InputStreamInputSource) {
+			try {
 				stream = ((InputStreamInputSource) source).getInputStream();
 				readXAR2(stream, filter, proxyFilter);
 			} catch (IOException e) {
-	        	this.logger.error("Fail to read XAR2 file descriptor.", e);
+				this.logger.error("Fail to read XAR2 file descriptor.", e);
 			}
-        } else {
-        	this.logger.error("Fail to read XAR2 file descriptor.");
-        }
-    }
-    private void readXAR2(InputStream stream,Object filter, XAR2InputFilter proxyFilter) throws IOException {
-        ZipArchiveInputStream zis = new ZipArchiveInputStream(stream, "UTF-8", false);
-        for (ZipArchiveEntry entry = zis.getNextZipEntry(); entry != null; entry = zis.getNextZipEntry()) {
-        	this.logger.info("Parsing file '" + entry.getName() + "'.");
-        }
-        zis.close();
-    }
+		} else {
+			this.logger.error("Fail to read XAR2 file descriptor.");
+		}
+	}
+
+	private void readXAR2(InputStream stream, Object filter,
+			XAR2InputFilter proxyFilter) throws IOException {
+		ZipArchiveInputStream zis = new ZipArchiveInputStream(stream, "UTF-8",
+				false);
+		for (ZipArchiveEntry entry = zis.getNextZipEntry(); entry != null; entry = zis
+				.getNextZipEntry()) {
+			this.logger.info("Parsing file '" + entry.getName() + "'.");
+			if (!entry.isDirectory()) {
+				String path = entry.getName();
+				String[] pathElements = path.split("/");
+				int length = pathElements.length;
+				String filename = pathElements[length - 1];
+				if (filename.endsWith("index.xml")) {
+					String documentName = null, spaceName = null, wikiName = null;
+					if (length >= 2) {
+						documentName = pathElements[length - 2];
+					}
+					if (length >= 3) {
+						spaceName = pathElements[length - 3];
+					}
+					if (length >= 4) {
+						wikiName = pathElements[length - 4];
+					}
+					try {
+						proxyFilter.beginWiki(wikiName,
+								FilterEventParameters.EMPTY);
+						proxyFilter.beginWikiSpace(wikiName,
+								FilterEventParameters.EMPTY);
+						proxyFilter.beginWikiDocument(documentName,
+								FilterEventParameters.EMPTY);
+						proxyFilter.endWikiDocument(documentName,
+								FilterEventParameters.EMPTY);
+						proxyFilter.endWikiSpace(wikiName,
+								FilterEventParameters.EMPTY);
+						proxyFilter.endWiki(wikiName,
+								FilterEventParameters.EMPTY);
+					} catch (FilterException e) {
+						String message = String.format(
+								"Problem with the import of the file '%s'.",
+								path);
+						logger.error(message, e);
+					}
+				}
+			}
+		}
+		zis.close();
+	}
 }
